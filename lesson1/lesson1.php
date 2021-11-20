04adce3c26c54b10ce1de15ec907e5ea99106575
alllekseev <oleg42482@gmail.com>
2021-11-20 23:55:26 +0300

    Lesson1 is ready

---
+++ lesson1/lesson1.php
@@ -0,0 +1,65 @@
+<?php
+// Примеры из урока
+
+// $name = "Bender";
+// $surname = "Rodriguez";
+// $age = 21;
+// echo "Hello, $name $surname! <br> Your age is $age. <br>";
+
+// define('MY_CONST', 100);
+// echo MY_CONST;
+
+// $a = 1;
+// echo "<br> $a <br>";
+// echo '$a';
+
+// $num = 5;
+// $b = true;
+// $b = (boolean)$b;
+// $str = 'Hello';
+// $c = $str . $num . $b;
+// echo $c;
+// $a = 1 % 2;
+// echo "$a";
+
+// $a = 4;
+// $b = 5;
+// $a += $b;
+// echo 'a='  . $a . '<br>';
+// $a = 0;
+// echo $a++ . '<br>';
+// echo ++$a . '<br>';
+// echo $a-- . '<br>';
+// echo --$a . '<br>';
+
+// $a = 4;
+// var_dump($a == $b);
+// var_dump($a === $b);
+// var_dump($a > $b);
+// var_dump($a < $b);
+
+// ----------------------------------------
+
+// Task 3;
+
+// $d = (int)1;
+// echo $d . "<br>";
+
+// $a = 5;
+// $b = '05';
+// var_dump($a == $b); // Сравнение по значению. 5 равняется 05;
+// var_dump((int)'012345'); // Потому что 0 в начале целого числа отбрасывается;
+// var_dump((float)123.0 === (int)123.0); // Типы данных не совпадают;
+// var_dump((int)0 === (int)'hello, world'); /*Сравнение по
+//                                     значению и по типу;
+//                                     фраза превращается
+//                                     в числовое значение 0; */
+
+// Task 5
+// $a = 1;
+// echo 'a=' . $a . '<br>';
+// $b = 2;
+// echo 'b=' . $b . '<br>';
+// echo 'a=' . ++$a . '<br>';
+// echo 'b=' . --$b . '<br>';
+?>
